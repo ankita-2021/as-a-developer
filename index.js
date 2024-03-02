287052705a897d8574fe0a1a64a7b0cbbc8e190f
Ankita Chakrawarti <80532826+ankita-2021@users.noreply.github.com>
2024-03-02 08:26:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
-//method-1
-// function callback() {
-//   console.log("hello node js");
-// }
+function whoAmI() {
+	console.log("I'm an actual programmer!");
+}
 
-// const add = function (a, b, callback) {
-//   var result = a + b;
-//   console.log("result", result);
-//   callback();
-// };
-// add(60, 40, callback);
-
-// //method-2
-// const add = function (a, b, callback) {
-//   var res = a + b;
-//   console.log("res", res);
-//   callback();
-// };
-// add(10, 15, function () {
-//   console.log("hello world");
-// });
-
-// //method-3
-// const add = function (a, b, callback) {
-//   var res = a + b;
-//   console.log("res", res);
-//   callback();
-// };
-// add(10, 12, () => console.log("hello node"));
-
-//create a server...
-const express = require("express");
-const app = express();
-
-app.get("/", function (req, res) {
-  res.send("Hello Node JS");
-});
-
-const port = 5000;
-app.listen(port, () => {
-  console.log(`server is listening on port${port}`);
-});
-
-// app.listen(port, function () {
-//   console.log(`server is listening on port${port}`);
-// });
+module.exports = whoAmI;
